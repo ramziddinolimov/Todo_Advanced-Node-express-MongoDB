f17aee77290cdb2103ea8e911560eadda8ed452f
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-09-12 16:22:43 +0500

    port mongodb

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+const MONGO_CONNECTION_STRING = "mongodb://localhost:27017";
 
 const client = new MongoClient(MONGO_CONNECTION_STRING);
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3030;
 const cookieParser = require("cookie-parser");
 const mongodb = require("./modules/mongo");
 const routes = require("./router/routes");
